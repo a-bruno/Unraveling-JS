e1b6499d7bcb459096dd0a783a41e466464d149a
a-bruno <brunogs1995@gmail.com>
2017-02-08 00:01:30 -0300

    operadores

---
+++ rd/12-operadores.js
@@ -0,0 +1,112 @@
+/*
+	Cuidado com operadores de comparação igual == e diferente.
+	O comportamento destes operadores pode produzir resultados 
+	inesperados por conta da coersão de tipos.
+*/
+
+0 == '' // true
+
+0 == '0' // true
+
+false == undefined // false
+
+false == null // false
+
+null == undefined // true
+
+1 == true // true
+
+0 == false // true
+
+0 == '\n' // true
+
+// Que mágica é essa chamada de coersão de tipos?
+
+/* 
+	Quando se faz comparação operandos que tem tipos diferentes,
+	ocorre a conversão de um dos dois operandos.
+*/
+
+10 == '10'
+
+/*
+	Se x é Number e y é String, a comparação será feita da seguinte forma:
+	x == ToNumber(y). Como o resultado de ToNumber('10') é 10, o retorno é true.
+*/
+
+null == undefined
+
+/*
+	Se x é null e y é undefined, o retorno é true.
+*/
+
+10 = new Number(10) // true
+
+var nota = new Number(10);
+
+console.log(nota); // {}
+
+console.log(nota.valueOf()); // 10
+
+10 == nota // true
+
+var x = {};
+
+10 == x; // false
+
+x.valueOf() = function() {
+	return 10;
+}
+
+console.log(x); // valueOf: [Function]
+
+10 == x; // true
+
+/*
+	Neste caso vai ocorrer uma operação abstrata, quem não podem ser acessadas
+	diretamente. São executadas pela própria linguagem.
+	Se x é Number e y é Object, a comparação será feita da seguinte forma:
+	x == ToPrimitive(y), que utiliza a operação valueOf, ou toString se 
+	não existir valueOf, do objeto para compará-lo com um Number. O retorno
+	da operação valeuOf nesse caso é 10, sendo assim o retorno é true.
+*/
+
+// Utilize os operadores muito igual === e muito diferente !==
+
+// Mas não seja assim tão radical, a coersão de tipos pode te ajudar.
+
+// Como fazer para comparar dois objetos em JavaScript?
+var x = {};
+
+var y = {};
+
+x == y; // false
+x === y; // false
+
+// false por que os objetos não tem a mesma referência.
+var z = x;
+
+x == z // true
+
+x === z // true
+
+/* 
+	Os operadores lógicos OU || e E && também escondem seus segredos. 
+	Além de ser curto-circuito, o retorno é um de seus operandos.
+*/
+
+0 || 2 é 2 // 2
+
+1 || 2 é 1 // 1
+
+1 && 2 é 2 // 2
+
+0 && 2 é 0 // 0
+
+if(10) {
+	console.log('Ok') // Ok
+}
+
+
+
+
