90ac30bdda555786c323e4ed1dbe852325ce1ef8
a-bruno <brunogs1995@gmail.com>
2017-02-09 23:35:51 -0300

    terminado heranca

--- rd/15-herancaP2.js
+++ rd/15-herancaP2.js
@@ -105,5 +105,19 @@ var pedro = _new(Homem, 'Pedro', 18);
 console.log(pedro);
 console.log(pedro.sexo);
 
+// Por dentro do new
+var _new = function(f) {
+	var res = {};
 
+	if (f.prototype !== null) {
+		res.__proto__ = f.prototype;
+	}
+
+	var ret = f.apply(res, Array.prototype.slice.call(arguments, 1));
+	if((typeof ret === 'object' || typeof ret === 'function') && ret !== null) {
+		return ret;
+	}
+
+	return res;
+};
 
